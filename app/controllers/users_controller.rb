03ed6984692079bb97fefb34a41ea73e76ab1f5f
GonzaVega <gonza.vega@icloud.com>
2022-06-18 20:05:44 -0300

    articles associated with users in show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
     def new
         @user = User.new
     end
