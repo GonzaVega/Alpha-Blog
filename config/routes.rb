0c46e3bf461a16e560b442dd19a84eb09dc08a0d
GonzaVega <gonza.vega@icloud.com>
2022-06-18 15:11:55 -0300

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :articles
   # only: [:show, :index, :new, :create, :edit, :update]. esto lo usamos cuando solo queremos mostrar algunas rutas, como quedo, muestra todas las posibles operaciones del backend.
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_131009) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_142307) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_131009) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
