bebebf21ea2b42ecc10d5710fcbe98617b97947d
GonzaVega <gonza.vega@icloud.com>
2022-06-20 17:27:20 -0300

    pagination added and listing articles change into cards

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,8 @@ class ArticlesController < ApplicationController
     def show
     end
     def index
-        @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 5)
+
     end
     def new
         @article = Article.new
