acf59b1708a275498d7db3319271d3401aeb9e92
GonzaVega <gonza.vega@icloud.com>
2022-07-06 12:28:40 -0300

    Search feature tidy up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     def show
         @articles = @user.articles.paginate(page: params[:page], per_page: 5) 
     end
+
     def search
         if params[:search].blank?
             flash[:alert] = "Please enter a search first"
@@ -14,6 +15,7 @@ class UsersController < ApplicationController
             @results = User.all.where("lower(username) LIKE :search", search: "%#{@parameter}%")
         end
     end
+
     def index
         @user = User.search(params[:search])
         @users = User.paginate(page: params[:page], per_page: 5) 
@@ -32,6 +34,7 @@ class UsersController < ApplicationController
             render 'edit'
         end
     end
+
     def create
         @user = User.new(user_params)
         if @user.save
@@ -42,19 +45,23 @@ class UsersController < ApplicationController
             render 'new'     
         end
     end
+
     def destroy
         @user.destroy
         session[:user_id] = nil if @user == current_user
         flash[:notice] = "Account and all associated articles where successfully deleted"
         redirect_to articles_path
     end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
     def set_user
         @user = User.find(params[:id])
     end
+    
     def require_same_user
         if current_user != @user && !current_user.admin?
             flash[:alert] = "You can only edit or delete your own account"
